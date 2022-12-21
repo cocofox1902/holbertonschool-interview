7804550419f8292046934e261e578d41e2fc02ad
cocofox1902 <colas.renard1902@gmail.com>
2022-12-21 14:39:52 +0100

    add

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -4,12 +4,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Creates a binary tree mode
- *
- * @parent: node parent
- * @value: node value
- * Return: pointer to the new node or NULL on failure
- */
+  * binary_tree_node - Creates a binary tree mode
+  *
+  * @parent: node parent
+  * @value: node value
+  * Return: pointer to the new node or NULL on failure
+  */
 
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
