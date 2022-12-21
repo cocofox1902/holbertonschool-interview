c2129b54d8a5ac7d6b248c093086aebb35671f58
cocofox1902 <colas.renard1902@gmail.com>
2022-12-21 13:51:35 +0100

    add chnage

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -3,12 +3,12 @@
 #include <string.h>
 #include "binary_trees.h"
 
-
 /* binary tree insertion node
  * @parent: parent node
  * @value: value to insert
  * Return: pointer to new node
  */
+
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
     binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
