951e7013ccad41023504afce6cf8b405c841d7c6
cocofox1902 <colas.renard1902@gmail.com>
2023-01-02 00:37:04 +0100

    add all task done

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+file creation minOperations
+"""
+
+def minOperations(n):
+    """
+    method minOperations
+    """
+    if n == 1 or n == 0:
+        return 0
+    i = 2
+    count = 0
+    while n > 1:
+        if n % i == 0:
+            n = n / i
+            count += i
+        else:
+            i += 1
+    return count   
