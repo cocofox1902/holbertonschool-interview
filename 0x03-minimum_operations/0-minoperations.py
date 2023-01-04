0f18f5a79d694514a730045b930fee9840d6cedb
cocofox1902 <colas.renard1902@gmail.com>
2023-01-04 01:53:42 +0100

    add test

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""a method that calculates the fewest number of operations needed to result
-   in exactly n H characters in the file"""
+"""
+file creation minOperations
+"""
 
 
 def minOperations(n):
     """
-        calculates the fewest number of operations
-        args:
-            n: the number of H desires in the file
-        return:
-            the number of operations needed to have the number of H
+    method minOperations
     """
-    nbOperation = 0
-    nbOfPass = 2
-    if n < 1:
+    if n == 1 or n == 0:
         return 0
+
+    i = 2
+    count = 0
+
     while n > 1:
-        if n % nbOfPass == 0:
-            n = n / nbOfPass
-            nbOperation += nbOfPass
+        if n % i == 0:
+            n = n / i
+            count += i
         else:
-            nbOfPass += 1
-    return nbOperation
+            i += 1
+    return count   
