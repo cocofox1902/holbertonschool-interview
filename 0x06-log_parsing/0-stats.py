790a81c568b94b3be1a7e3ead916f76278e24bb8
cocofox1902 <colas.renard1902@gmail.com>
2023-01-26 15:25:05 +0100

    add all

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -7,24 +7,21 @@ status_codes = {code: 0 for code in [
     "200", "301", "400", "401", "403", "404", "405", "500"]}
 count = 0
 
-try:
-    for line in stdin:
-        try:
-            args = line.strip().split(" ")
-            if len(args) == 9:
-                if args[-2] in status_codes:
-                    status_codes[args[-2]] += 1
-                total_size += int(args[-1])
-                count += 1
-                if count % 10 == 0:
-                    print("File size: {}".format(total_size))
-                    for status, count in sorted(status_codes.items()):
-                        if count != 0:
-                            print("{}: {}".format(status, count))
-        except ValueError:
-            pass
-except KeyboardInterrupt:
-    pass
+for line in stdin:
+    try:
+        args = line.strip().split(" ")
+        if len(args) == 9:
+            if args[-2] in status_codes:
+                status_codes[args[-2]] += 1
+            total_size += int(args[-1])
+            count += 1
+            if count % 10 == 0:
+                print("File size: {}".format(total_size))
+                for status, count in sorted(status_codes.items()):
+                    if count != 0:
+                        print("{}: {}".format(status, count))
+    except ValueError:
+        pass
 
 print("File size: {}".format(total_size))
 for status, count in sorted(status_codes.items()):
