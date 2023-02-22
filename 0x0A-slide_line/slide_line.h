9dcde932b021d6069ebdf1bca8a7c496bf7df640
cocofox1902 <colas.renard1902@gmail.com>
2023-02-22 09:26:57 +0100

    add all

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -7,5 +7,7 @@
 #define SLIDE_RIGHT 1
 
 int slide_line(int *line, size_t size, int direction);
+int slide_left(int *line, size_t size);
+int slide_right(int *line, size_t size);
 
 #endif /* SLIDE_LINE_H */
