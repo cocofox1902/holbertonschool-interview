b3e00f178a382bdf0508aba6d196ac51b782fc93
cocofox1902 <colas.renard1902@gmail.com>
2023-04-12 13:33:05 +0200

    add all

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,24 +1,24 @@
 #!/usr/bin/node
-/* Script List characters of Star Wars movie
-*/
+const movieId = process.argv[2];
 const request = require('request');
-const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
-request(url, async function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    for (const c in characters) {
-      const response = await new Promise((resolve, reject) => {
-        request(characters[c], (error, response, body) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(JSON.parse(body).name);
-          }
-        });
+const data = JSON.parse(response.body);
+const charsData = data.characters;
+
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+request(url, async (err, response) => {
+  if (err) {
+    console.error(err);
+  }
+  for (const charId in charsData) {
+    await new Promise((resolve, reject) => {
+      request(charsData[charId], function (err, response) {
+        if (err) {
+          console.log(err);
+        }
+        const charData = JSON.parse(response.body);
+        console.log(charData.name);
+        resolve();
       });
-      console.log(response);
-    }
+    });
   }
 });
