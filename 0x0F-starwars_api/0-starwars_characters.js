52dd2b09ce4526240594427a9b66345607ea3831
cocofox1902 <colas.renard1902@gmail.com>
2023-04-12 13:36:19 +0200

    add all

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,33 +1,24 @@
 #!/usr/bin/node
-
-/*
-JavaScript script that prints all characters of a Star Wars movie
-API service: https://intranet.hbtn.io/rltoken/aiMsg1QkH-FuPn7gyo9O6A
-use the request module
+/* Script List characters of Star Wars movie
 */
-
 const request = require('request');
-const movieId = process.argv[2];
-
-// Request URL
-const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-
-request(url, async (error, response, body) => {
-  if (error) console.log(error);
-
-  const characters = JSON.parse(body).characters;
-
-  for (const character in characters) {
-    const urlChar = await characters[character];
-
-    const name = await new Promise((resolve, reject) => {
-      request(urlChar, (error, response, body) => {
-        if (error) console.log(error);
-
-        resolve(JSON.parse(body).name);
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request(url, async function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const c in characters) {
+      const response = await new Promise((resolve, reject) => {
+        request(characters[c], (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body).name);
+          }
+        });
       });
-    });
-
-    console.log(name);
+      console.log(response);
+    }
   }
 });
