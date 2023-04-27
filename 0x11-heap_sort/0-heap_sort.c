8367f4bc18827f40e43d3484cf7e8a3f9e1cbae4
cocofox1902 <colas.renard1902@gmail.com>
2023-04-27 20:26:51 +0200

    add all

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -9,31 +9,34 @@
  */
 void sift_down(int *array, size_t start, size_t end, size_t size)
 {
-    size_t root = start;
-    size_t child, swap;
+	size_t root = start;
+	size_t child, swap;
 
-    while (2 * root + 1 <= end)
-    {
-        child = 2 * root + 1;
-        swap = root;
+	while (2 * root + 1 <= end)
+	{
+		child = 2 * root + 1;
+		swap = root;
 
-        if (array[swap] < array[child])
-            swap = child;
+		if (array[swap] < array[child])
+			swap = child;
 
-        if (child + 1 <= end && array[swap] < array[child + 1])
-            swap = child + 1;
+		if (child + 1 <= end && array[swap] < array[child + 1])
+			swap = child + 1;
 
-        if (swap == root)
-            return;
-        else
-        {
-            int tmp = array[root];
-            array[root] = array[swap];
-            array[swap] = tmp;
-            print_array(array, size);
-            root = swap;
-        }
-    }
+		if (swap == root)
+		{
+			return;
+		}
+		else
+		{
+			int tmp = array[root];
+
+			array[root] = array[swap];
+			array[swap] = tmp;
+			print_array(array, size);
+			root = swap;
+		}
+	}
 }
 
 /**
@@ -44,21 +47,21 @@ void sift_down(int *array, size_t start, size_t end, size_t size)
  */
 void heap_sort(int *array, size_t size)
 {
-    int tmp;
-    size_t end;
+	int tmp;
+	size_t end;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (end = (size - 2) / 2; (int)end >= 0; end--)
-        sift_down(array, end, size - 1, size);
+	for (end = (size - 2) / 2; (int)end >= 0; end--)
+		sift_down(array, end, size - 1, size);
 
-    for (end = size - 1; end > 0; end--)
-    {
-        tmp = array[end];
-        array[end] = array[0];
-        array[0] = tmp;
-        print_array(array, size);
-        sift_down(array, 0, end - 1, size);
-    }
+	for (end = size - 1; end > 0; end--)
+	{
+		tmp = array[end];
+		array[end] = array[0];
+		array[0] = tmp;
+		print_array(array, size);
+		sift_down(array, 0, end - 1, size);
+	}
 }
