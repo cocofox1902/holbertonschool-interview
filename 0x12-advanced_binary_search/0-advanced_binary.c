51d23a8ec44514d20ebb58e8b6f0969270e5332e
cocofox1902 <colas.renard1902@gmail.com>
2023-05-03 14:22:24 +0200

    add all

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -1,36 +1,45 @@
 #include <stdio.h>
 
+/**
+ * advanced_binary - searches for a value in a sorted array of integers
+ * using the Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: index where value is located or -1 if value is not present
+*/
+
 int advanced_binary(int *array, size_t size, int value)
 {
-    if (array == NULL || size == 0)
-        return (-1);
+	if (array == NULL || size == 0)
+		return (-1);
 
-    int low = 0, high = size - 1;
+	int low = 0, high = size - 1;
 
-    while (low <= high)
-    {
-        int mid = (low + high) / 2;
-        printf("Searching in array: ");
-        for (size_t i = low; i <= high; i++)
-        {
-            printf("%d", array[i]);
-            if (i != high)
-                printf(", ");
-        }
-        printf("\n");
+	while (low <= high)
+	{
+		int mid = (low + high) / 2;
 
-        if (array[mid] == value)
-        {
-            if (mid == 0 || array[mid - 1] != value)
-                return (mid);
-            else
-                high = mid - 1;
-        }
-        else if (array[mid] > value)
-            high = mid - 1;
-        else
-            low = mid + 1;
-    }
+		printf("Searching in array: ");
+		for (size_t i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i != high)
+				printf(", ");
+		}
+		printf("\n");
 
-    return (-1);
+		if (array[mid] == value)
+		{
+			if (mid == 0 || array[mid - 1] != value)
+				return (mid);
+			high = mid - 1;
+		}
+		else if (array[mid] > value)
+			high = mid - 1;
+		else
+			low = mid + 1;
+	}
+
+	return (-1);
 }
