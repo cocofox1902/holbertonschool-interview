51ee0f504b32949245fd830102dafd2ba0b2fd96
cocofox1902 <colas.renard1902@gmail.com>
2023-06-22 15:41:57 +0200

    add all

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_array(const int *array, size_t size);
 void merge(int *holder, int *array, int mid, size_t size);
 void merge_sort(int *array, size_t size);
 void merge_sort_holder(int *array, size_t size, int *holder);
