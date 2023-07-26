11b63402f33d0b49e388ea9f0521e398c8121e83
cocofox1902 <colas.renard1902@gmail.com>
2023-07-26 11:23:18 +0200

    add all

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Island Perimeter
+"""
+
 
 def island_perimeter(grid):
     perimeter = 0
@@ -10,11 +13,9 @@ def island_perimeter(grid):
             if grid[i][j] == 1:
                 perimeter += 4
 
-                # Check left cell
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
 
-                # Check above cell
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
 
